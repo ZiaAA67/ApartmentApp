810296db3510fc656226ed110cf86110cb74094c
Nghia <nghialntilactuca@gmail.com>
2025-03-25 17:17:11 +0700

    new project

---
+++ ApartmentManagement/src/main/java/com/mntn/configs/WebAppContextConfigs.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mntn.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author nghia
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.mntn.controllers",
+    "com.mntn.repositories",
+    "com.mntn.services"
+})
+
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+}
---
+++ ApartmentManagement/src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Mar 23, 2025, 9:29:01 PM
+    Author     : nghia, nhat
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Trang chủ</title>
+    </head>
+    <body>
+        <h1>${message}</h1>
+    </body>
+</html>
